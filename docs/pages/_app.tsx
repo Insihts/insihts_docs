cc14782f342aa93064e36ef3b4a7f7aa1a1fe56d
Abhishek Singh <77458371+7hakurg@users.noreply.github.com>
2025-01-04 22:25:33 -0800

    a

--- docs/pages/_app.tsx
+++ docs/pages/_app.tsx
@@ -1,6 +1,7 @@
 import type { AppProps } from 'next/app'
 import type { ReactElement } from 'react'
 import '../style.css'
+import * as NextraThemeDocs from 'nextra-theme-docs';
 
 export default function App({ Component, pageProps }: AppProps): ReactElement {
   return <Component {...pageProps} />
